6ad0fe731adcdb9cde7d6c51ef56f40a58e15085
Alex Bilik <alex.bilik@intel.com>
2023-08-24 13:32:13 +0300

    Create mountain_build.groovy

---
+++ mountain_build.groovy
@@ -0,0 +1,50 @@
+Boolean STAGE1 = true
+Boolean STAGE2 = true
+Boolean STAGE3 = true
+
+pipeline {
+    agent { label "AGENT_LABEL" }
+
+    options {
+        timestamps()
+        parallelsAlwaysFailFast()
+    }
+    parameters {
+        string(name: 'REPOS', defaultValue: '', description: '')
+        string(name: 'VERSION', defaultValue: '', description: '')
+        booleanParam(name: 'EXECUTE_STAGE1', defaultValue: '', description: '')
+        booleanParam(name: 'EXECUTE_STAGE2', defaultValue: '', description: '')
+        booleanParam(name: 'EXECUTE_STAGE3', defaultValue: '', description: '')
+    }
+    stages {
+
+        stage('Build project jobs') {
+            steps {
+                script {
+                    
+                    echo("Got value REPOS = ${params.REPOS}")
+                    echo("Got value VERSION = ${params.VERSION}")
+
+                    if (params.EXECUTE_STAGE1 == true) {
+                      echo ("STAGE1 is True")
+                    }
+                  
+                    if (params.EXECUTE_STAGE2 == true) {
+                      echo ("STAGE2 is True")
+                    }
+                  
+                    if (params.EXECUTE_STAGE3 == true) {
+                      echo ("STAGE3 is True")
+                    }
+                } //script
+            } //steps
+        } //stage
+    }
+    post {
+        cleanup {
+            script {
+                cleanWs(deleteDirs: true, notFailBuild: true)
+            }
+        }
+    }
+} // pipeline
